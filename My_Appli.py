b6eea0bbaa58556b9145b8c585fcc20669d7f842
BenzThanaporn <6540081322@student.chula.ac.th>
2023-12-11 01:41:54 +0700

    Update My_Appli.py

--- My_Appli.py
+++ My_Appli.py
@@ -1,1 +1,11 @@
 #Child Naming Application
+
+import streamlit as st
+import openai
+import json
+import pandas as pd
+
+# Get the API key from the sidebar called OpenAI API key
+user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
+
+
