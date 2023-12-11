2781a29c39317b268e1d885e2db829e7d4b58a5a
BenzThanaporn <6540081322@student.chula.ac.th>
2023-12-11 14:26:19 +0700

    Update My_Appli.py

--- My_Appli.py
+++ My_Appli.py
@@ -9,19 +9,18 @@ import pandas as pd
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
 client = openai.OpenAI(api_key=user_api_key)
-prompt = """Act as an AI writing tutor in English. You will receive a 
-            piece of writing and you should give suggestions on how to improve it.
-            List the suggestions in a JSON array, one suggestion per line.
-            Each suggestion should have 3 fields:
-            - "before" - the text before the suggestion
-            - "after" - the text after the suggestion
-            - "category" - the category of the suggestion one of "grammar", "style", "word choice", "other"
-            - "comment" - a comment about the suggestion
-            Don't say anything at first. Wait for the user to say something.
-        """  
+prompt = """
+        Act as a writer. 
+        You will recieve words and you should write a story from these words.
+        And then make 7 question from the story you wrote in a JSON array, one question per line.
+        Each question should have 3 fields:
+        - "Question" -the question you make from the story
+        - "Answer" -the answer for the question
+        """
         
-st.title('Name Your Child')
-st.markdown('Input the writing that you want to improve. \n\
-            The AI will give you suggestions on how to improve it.')
+st.title('Write a story and Make questions')
+st.markdown('Input 2-10 words.\n\
+            The AI will write a story from the words you gave \n\
+            and then you wil get 10 questions from the story, along with their answer.')
 
-user_input = st.text_area("Enter some text to correct:", "Your text here")
+user_input = st.text_area("Enter 2-10 words.Use colon to separate each words.\nFor the example: bee, flower, eat", "Put 2-10 words")
