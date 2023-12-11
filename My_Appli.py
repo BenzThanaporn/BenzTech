4c7fd78c7c2faa851dac92fe0d76e7aec7f9e043
BenzThanaporn <6540081322@student.chula.ac.th>
2023-12-11 16:07:41 +0700

    Update My_Appli.py

--- My_Appli.py
+++ My_Appli.py
@@ -1,5 +1,3 @@
-#Write a story from user's words and make questions
-
 import streamlit as st
 import openai
 import json
@@ -10,39 +8,36 @@ user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
 client = openai.OpenAI(api_key=user_api_key)
 prompt = """
-        Act as a writer. 
-        You will recieve words and you should write a story from these words.
-        And then make 7 question from the story you wrote in a JSON array, one question per line.
-        Each question should have 3 fields:
-        - "Question" -the question you make from the story
-        - "Answer" -the answer for the question
-        """
-        
+    Act as a writer. 
+    You will recieve words and you should write a story from these words.
+    And then make 7 question from the story you wrote in a JSON array, one question per line.
+    Each question should have 3 fields:
+    - "Question" -the question you make from the story
+    - "Answer" -the answer for the question
+    """
+    
 st.title('Write a story and Make questions')
 st.markdown('Input your words.\n\
-            The AI will write a story from the words you gave \n\
-            and then you wil get 10 questions from the story, along with their answer.')
+        The AI will write a story from the words you gave \n\
+        and then you wil get 10 questions from the story, along with their answer.')
 
 user_input = st.text_area("Enter 2-10 words. Use the colon to separate each words. For the example: bee, flower, eat")
 
+# submit button after text input
+if st.button('Submit'):
+    messages_so_far = [
+        {"role": "system", "content": prompt},
+        {'role': 'user', 'content': user_input},
+    ]
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages_so_far
+    )
+    st.info(response['choices'][0]['message']['content'])
+    
 
 
-if st.button("Submit"):
-    if user_input:
-        response = client.complete(
-            engine="text-davinci-003",
-            prompt=prompt + "\n\n" + user_input,
-            max_tokens=100,
-            n=1,
-            stop=None,
-            temperature=0.7,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0,
-            log_level="info",
-        )
-        output = response.choices[0].text.strip()
-        st.text_area("Output", value=output, height=200)
-    else:
-        st.warning("Please enter some words before submitting.")
+
+
+
 
