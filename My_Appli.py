3999aa2d75d72eac39e309c029378222deb6fac3
BenzThanaporn <6540081322@student.chula.ac.th>
2023-12-11 17:38:27 +0700

    Update My_Appli.py

--- My_Appli.py
+++ My_Appli.py
@@ -2,49 +2,28 @@ import streamlit as st
 import openai
 import json
 import pandas as pd
+import requests
 
 # Get the API key from the sidebar called OpenAI API key
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
-#user_api_key = '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'
-#user_api_key = 'YOUR_API_KEY_HERE'  # Replace with your actual API key
-#client = openai.ChatCompletion.create(api_key=user_api_key)
-
-#user_api_key = 'YOUR_API_KEY_HERE'  # Replace with your actual API key
-#client = openai.api_key = user_api_key
 
 client = openai.OpenAI(api_key=user_api_key)
-prompt = """Act as a Korean-English-Thai translator. You will receive a 
-            Korean text and you should give the translation in English and Thai.
-            You will translate the text into English first and then translate the English text into Thai.
-            If the given text is already in English, you should translate it into Thai directly.
-            List the translation in a JSON array, one translation per line.
-            Each translation should have 3 fields:
-            - "Korean Original Text" - the Korean original text before the translation
-            - "English Translation" - the text translated into English
-            - "Thai Translation" - the text translated into Thai
-            For example:
-            [
-                {
-                    "Korean Original Text": "안녕하세요. 저는 한국어를 배우고 있습니다.",
-                    "English Translation": "Hello. I am learning Korean.",
-                    "Thai Translation": "สวัสดีค่ะ ฉันกำลังเรียนภาษาเกาหลี"
-                },
-                {
-                    "Korean Original Text": "안녕하세요. 저는 한국어를 배우고 있습니다.",
-                    "English Translation": "Hello. I am learning Korean.",
-                    "Thai Translation": "สวัสดีค่ะ ฉันกำลังเรียนภาษาเกาหลี"
-                }
-            ]
-        """    
+prompt = """
+    Act as a writer. 
+    You will recieve words and you should write a story from these words.
+    Tell me the story.
+    And then make 7 question in a JSON array from the story you wrote, one question per line.
+    Each question should have 3 fields:
+    - "Question" -the question you make from the story
+    - "Answer" -the answer for the question
+    """
+    
+st.title('Write a story and Make questions')
+st.markdown('Input your words.\n\
+        The AI will write a story from the words you gave \n\
+        and then you wil get 10 questions from the story, along with their answer.')
 
-
-st.title('We go Korean')
-st.subheader('Korean-English-Thai translator')
-st.markdown('Input the text that you want to translate. \n\
-            The AI will translate it for you.')
-
-user_input = st.text_area("Enter some text to correct:", "Your text here")
-
+user_input = st.text_area("Enter 2-10 words. Use the colon to separate each words. For the example: bee, flower, eat")
 
 # submit button after text input
 if st.button('Submit'):
