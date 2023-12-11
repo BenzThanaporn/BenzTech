d654a52fa826a2a3986829eb9502abf7535881e7
BenzThanaporn <6540081322@student.chula.ac.th>
2023-12-11 16:24:01 +0700

    Update My_Appli.py

--- My_Appli.py
+++ My_Appli.py
@@ -2,6 +2,7 @@ import streamlit as st
 import openai
 import json
 import pandas as pd
+import requests
 
 # Get the API key from the sidebar called OpenAI API key
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
