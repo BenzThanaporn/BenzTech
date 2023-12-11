3b8b5893ca48e3eb6e7e879d759ce94e1d7c8273
BenzThanaporn <6540081322@student.chula.ac.th>
2023-12-11 14:28:56 +0700

    Update My_Appli.py

--- My_Appli.py
+++ My_Appli.py
@@ -23,4 +23,4 @@ st.markdown('Input 2-10 words.\n\
             The AI will write a story from the words you gave \n\
             and then you wil get 10 questions from the story, along with their answer.')
 
-user_input = st.text_area("Enter 2-10 words.Use colon to separate each words.\nFor the example: bee, flower, eat", "Put 2-10 words")
+user_input = st.text_area("Enter 2-10 words.Use colon to separate each words.\n\For the example: bee, flower, eat", "Put your words here")
