72a267f5ad5f78fe8bcffc0952c0fb06b7700539
BenzThanaporn <6540081322@student.chula.ac.th>
2023-12-11 22:33:34 +0700

    Update My_Appli.py

--- My_Appli.py
+++ My_Appli.py
@@ -2,28 +2,31 @@ import streamlit as st
 import openai
 import json
 import pandas as pd
-import requests
 
 # Get the API key from the sidebar called OpenAI API key
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
 client = openai.OpenAI(api_key=user_api_key)
 prompt = """
-    Act as a writer. 
-    You will recieve words and you should write a story from these words.
-    Tell me the story.
-    And then make 7 question in a JSON array from the story you wrote, one question per line.
-    Each question should have 3 fields:
-    - "Question" -the question you make from the story
-    - "Answer" -the answer for the question
+    Act as an AI tutor in English.
+    You will receive an English passage and you should make 7 questions from the passage.
+    List the questions in a JSON array, one question per line.
+    Each question should have 5 fields :
+    - “Question” - the question you make from the passage
+    - “Answer” - the answer for the question
+    - “Interesting word” - give 1 interesting word from the question
+    - “Part of speech” - the interesting word's part of speech
+    - “Translation in Thai” - the interesting word's translation in Thai
+    The interesting word must be unique in each question.
     """
     
-st.title('Write a story and Make questions')
-st.markdown('Input your words.\n\
-        The AI will write a story from the words you gave \n\
-        and then you wil get 10 questions from the story, along with their answer.')
+st.title("Let's learn from English passage")
+st.markdown("Input your English passage.\n\
+        The AI will make 7 questions from the passage you gave, along with their answers.\n\
+        In each question, 1 interesting word and its part of speech and its translation in Thai will also be provided.\n\
+        The interesting word is the word that you should learn from the question.")
 
-user_input = st.text_area("Enter 2-10 words. Use the colon to separate each words. For the example: bee, flower, eat")
+user_input = st.text_area("Enter passage to learn", "Put English passage here")
 
 # submit button after text input
 if st.button('Submit'):
@@ -37,15 +40,15 @@ if st.button('Submit'):
     )
     # Show the response from the AI in a box
     st.markdown('**AI response:**')
-    suggestion_dictionary = response.choices[0].message.content
+    result_dictionary = response.choices[0].message.content
 
 
-    sd = json.loads(suggestion_dictionary)
+    rd = json.loads(result_dictionary)
 
-    print (sd)
-    suggestion_df = pd.DataFrame.from_dict(sd)
-    print(suggestion_df)
-    st.table(suggestion_df)
+    print (rd)
+    result_df = pd.DataFrame.from_dict(rd)
+    print(result_df)
+    st.table(result_df)
     
 
 
